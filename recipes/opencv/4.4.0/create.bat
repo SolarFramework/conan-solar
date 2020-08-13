41cb26e502a7fb6ba1e27ecb12e9b438c14a7ff2
BCOM\jroyan <jerome.royan@b-com.com>
2020-08-13 16:58:20 +0200

    Add --build=missing and cppstd 17 to create batch for Opencv recipe

--- recipes/opencv/4.4.0/create.bat
+++ recipes/opencv/4.4.0/create.bat
@@ -1,7 +1,7 @@
 @echo on
-conan create . conan-solar/stable -tf None 
-conan create . conan-solar/stable -tf None -s build_type=Debug
-conan create . conan-solar/stable -tf None -o contrib=True -o nonfree=True -o harfbuzz=False
-conan create . conan-solar/stable -tf None -s build_type=Debug -o contrib=True -o nonfree=True -o harfbuzz=False
+conan create . conan-solar/stable -tf None --build=missing -s compiler.cppstd=17 -o shared=True
+conan create . conan-solar/stable -tf None --build=missing -s compiler.cppstd=17 -s build_type=Debug -o shared=True
+conan create . conan-solar/stable -tf None --build=missing -s compiler.cppstd=17 -o shared=True -o contrib=True -o nonfree=True -o harfbuzz=False -o gflags=False -o glog=False
+conan create . conan-solar/stable -tf None --build=missing -s compiler.cppstd=17 -s build_type=Debug  -o shared=True -o contrib=True -o nonfree=True -o harfbuzz=False -o gflags=False -o glog=False
 
 
