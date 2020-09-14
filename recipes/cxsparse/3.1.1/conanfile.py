84e29e8b3407bec84f7b84e2267e5912f275fc78
BCOM\jroyan <jerome.royan@b-com.com>
2020-09-14 10:15:08 +0200

    Update recipe cxsparse pour buidl Android et Linux

--- recipes/cxsparse/3.1.1/conanfile.py
+++ recipes/cxsparse/3.1.1/conanfile.py
@@ -1,71 +1,66 @@
-from conans import ConanFile, CMake, tools
 import os
 import shutil
+from conans import ConanFile, tools, CMake
+from conans.errors import ConanInvalidConfiguration
 
-
-class LibCxsparseConan(ConanFile):
+class cxsparsesolverConan(ConanFile):
     name = "cxsparse"
-    package_revision = ""
-    upstream_version = "3.1.1"
-    version = "{0}{1}".format(upstream_version, package_revision)
-    generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
-    exports = [
-        "patches/Demo/CMakeLists.txt",
-        "patches/CMakeLists.txt",
-        "patches/cs_convert.c.diff",
-        "patches/SuiteSparse_config.h.diff",
-        "patches/FindCXSparse.cmake"
-    ]
-    url = "https://github.com/Solar-Framework/conan-solar/recipes/cxsparse/3.1.1"
+    filename = "CXSparse"
     license = "GNU Lesser General Public License"
-    description = "A concise sparse Cholesky library."
-    source_subfolder = "source_subfolder"
-    short_paths = True
+    url = "https://github.com/Solar-Framework/conan-solar/recipes/cxsparse/3.1.1"
+    homepage = "https://github.com/PetterS/CXSparse"
+    description = ("A concise sparse Cholesky library.")
+    topics = ("optimization","Non-linear Least Squares")
+    settings = "os", "arch", "compiler", "build_type"
+    generators = ["cmake", "cmake_find_package"]
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    exports_sources = ["CMakeLists.txt","patches/*"]
+    
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
 
     def configure(self):
-        if not tools.os_info.is_windows:
-            self.options.shared = False
-
-    def requirements(self):
-        self.requires("common/1.0.2@conan-solar/stable")
-
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+			
     def source(self):
-        tools.get("https://github.com/PetterS/CXSparse/archive/{0}.tar.gz".format(self.upstream_version))
-        os.rename("CXSparse-" + self.upstream_version, self.source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename("%s-%s" % (self.filename, self.version), self._source_subfolder)
 
     def build(self):
-        cxsparse_source_dir = os.path.join(self.source_folder, self.source_subfolder)
-        shutil.copy("patches/Demo/CMakeLists.txt",
-                    os.path.join(cxsparse_source_dir, "Demo", "CMakeLists.txt"))
+        #Make sure that cmake finds gflags is use_gflags=True
+        #tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+         #                     "find_package(Gflags)",
+          #                    "find_package(Gflags REQUIRED)")
+        #On windows the library names can be gflags.dll or gflags_static.lib
+        #tools.replace_in_file(os.path.join(self._source_subfolder, "cmake", "FindGflags.cmake"),
+        #                      "find_library(GFLAGS_LIBRARY NAMES gflags",
+        #                      "find_library(GFLAGS_LIBRARY NAMES gflags gflags_static")
+        cxsparse_source_dir = os.path.join(self.source_folder, "source_subfolder")
         shutil.copy("patches/CMakeLists.txt",
-                    os.path.join(cxsparse_source_dir, "CMakeLists.txt"))
-        tools.patch(cxsparse_source_dir, "patches/cs_convert.c.diff")
-        tools.patch(cxsparse_source_dir, "patches/SuiteSparse_config.h.diff")
-
-        # Import common flags and defines
-        import common
-
-        # Generate Cmake wrapper
-        common.generate_cmake_wrapper(
-            cmakelists_path='CMakeLists.txt',
-            source_subfolder=self.source_subfolder,
-            build_type=self.settings.build_type
-        )
-
+            os.path.join(cxsparse_source_dir, "CMakeLists.txt"))
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
         cmake = CMake(self)
-
-        if not tools.os_info.is_windows:
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        if self.settings.os == "Windows":
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
-
-        cmake.configure()
+        cmake.configure(build_folder=self._build_subfolder)
         cmake.build()
         cmake.install()
 
     def package(self):
         self.copy("FindCXSparse.cmake", src="patches", dst=".", keep_path=False)
-
+		
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        
+        
