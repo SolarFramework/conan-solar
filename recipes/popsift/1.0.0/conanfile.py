612d179269d4a259e0fbb856fee54ccb1b4e6abc
BCOM\jroyan <jerome.royan@b-com.com>
2023-04-24 16:47:50 +0200

    Update PopSift recipe to support conan V2 and CUDA 12.

--- recipes/popsift/1.0.0/conanfile.py
+++ recipes/popsift/1.0.0/conanfile.py
@@ -1,11 +1,17 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile
 from conans.errors import ConanInvalidConfiguration
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, collect_libs, copy, export_conandata_patches, get, rename, replace_in_file, rmdir, save
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 import os
+import re
+import textwrap
 
-required_conan_version = ">=1.29.1"
+required_conan_version = ">=1.54.0"
 
 
-class ColmapConan(ConanFile):
+class PopsiftConan(ConanFile):
     name = "popsift"
     license = "MPL v2 license"
     homepage = "https://github.com/alicevision/popsift/"
@@ -15,19 +21,26 @@ class ColmapConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
-    exports_sources = ["CMakeLists.txt", "patches/**"]
-    source_subfolder = "source_subfolder"
-    generators = "cmake", "cmake_find_package"
-    _cmake = None
+#    generators = "cmake", "cmake_find_package"
+#    _cmake = None
 
-    def configure(self):
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+#   def configure(self):
         # it is also necessary to remove the VS runtime
-        if self.settings.compiler == "Visual Studio":
-            del self.settings.compiler.runtime
+#        if self.settings.compiler == "Visual Studio":
+#            del self.settings.compiler.runtime
+            
+    def validate(self):
+        if self.options.shared and is_msvc(self) and is_msvc_static_runtime(self):
+            raise ConanInvalidConfiguration("Visual Studio with static runtime is not supported for shared library.")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename("popsift-{}".format(self.version), self.source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     @property
     def _android_arch(self):
@@ -35,28 +48,31 @@ class ColmapConan(ConanFile):
         return tools.to_android_abi(arch)
 
     def _patch_sources(self):
-        for patch in self.conan_data["patches"][self.version]:
-            tools.patch(**patch)
+        apply_conandata_patches(self)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+
+        tc.variables["PopSift_BUILD_EXAMPLES"] = 'OFF'
+        
+        if is_msvc(self):
+            tc.variables["BUILD_WITH_STATIC_CRT"] = is_msvc_static_runtime(self)
+        
+        tc.generate()
+
+        CMakeDeps(self).generate()
+        
     def build(self):
         self._patch_sources()
-        popsift_source_dir = os.path.join(self.source_folder, "source_subfolder")
 
         cmake = CMake(self)
-
-        if self.options.shared :
-            cmake.definitions["BUILD_SHARED_LIBS"] = 'ON'
-        else :
-            cmake.definitions["BUILD_SHARED_LIBS"] = 'OFF'
-
-        cmake.definitions["PopSift_BUILD_EXAMPLES"] = 'OFF'
-
         cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
 
     def package(self):
         # Retrieve common helpers
@@ -64,7 +80,8 @@ class ColmapConan(ConanFile):
 
         # Fix all hard coded path to conan package in all .cmake files
 #        common.fix_conan_path(self, self.package_folder, '*.cmake')
-        
+        cmake = CMake(self)
+        cmake.install()
         if self.settings.os == 'Android':
             if not self.options.shared:
                 self.cpp_info.includedirs.append(
