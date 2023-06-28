cc11d82f12631dc64850c05fca8bbbc855c475c9
BCOM\jroyan <jerome.royan@b-com.com>
2023-06-29 00:52:21 +0200

    Add all cuda arch configuration to opencv create scripts

--- recipes/opencv/4.x/create.sh
+++ recipes/opencv/4.x/create.sh
@@ -3,5 +3,8 @@ conan create . opencv/4.5.5@ -tf None --build=missing -s compiler.cppstd=17 -o s
 conan create . opencv/4.5.5@ -tf None --build=missing -s compiler.cppstd=17 -s build_type=Debug -o shared=True
 conan create . opencv/4.5.5@ -tf None --build=missing -s compiler.cppstd=17 -o shared=True -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True -o cuda_arch_bin="75 80 86"
 conan create . opencv/4.5.5@ -tf None --build=missing -s compiler.cppstd=17 -s build_type=Debug  -o shared=True -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True -o cuda_arch_bin="75 80 86"
+conan create . opencv/4.5.5@ -tf None --build=missing -s compiler.cppstd=17 -o shared=True -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True
+conan create . opencv/4.5.5@ -tf None --build=missing -s compiler.cppstd=17 -s build_type=Debug  -o shared=True -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True
+
 
 
--- recipes/opencv/4.x/createWindows.bat
+++ recipes/opencv/4.x/createWindows.bat
@@ -2,3 +2,5 @@ conan create . opencv/4.5.5@ --build=missing -tf None -o shared=True -o with_ffm
 conan create . opencv/4.5.5@ --build=missing -tf None -s build_type=Debug -o shared=True -o with_ffmpeg=False
 conan create . opencv/4.5.5@ --build=missing -tf None -o shared=True -o with_ffmpeg=False -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True -o cuda_arch_bin="75 80 86"
 conan create . opencv/4.5.5@ --build=missing -tf None -s build_type=Debug -o shared=True -o with_ffmpeg=False -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True -o cuda_arch_bin="75 80 86"
+conan create . opencv/4.5.5@ --build=missing -tf None -o shared=True -o with_ffmpeg=False -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True
+conan create . opencv/4.5.5@ --build=missing -tf None -s build_type=Debug -o shared=True -o with_ffmpeg=False -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True
