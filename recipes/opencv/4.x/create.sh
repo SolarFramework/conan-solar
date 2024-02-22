ac79a1356881334064f43c73eb157c00ccae9831
granger35 <jerome.royan@b-com.com>
2024-02-22 12:57:29 +0100

    update some create scripts for conan V2

--- recipes/opencv/4.x/create.sh
+++ recipes/opencv/4.x/create.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-conan create . opencv/4.7.0@conan-solar/1_1_0 -tf None --build=missing -s compiler.cppstd=17
-conan create . opencv/4.7.0@conan-solar/1_1_0 -tf None --build=missing -s compiler.cppstd=17 -s build_type=Debug
-conan create . opencv/4.7.0@conan-solar/1_1_0 -tf None --build=missing -s compiler.cppstd=17 -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True
-conan create . opencv/4.7.0@conan-solar/1_1_0 -tf None --build=missing -s compiler.cppstd=17 -s build_type=Debug -o contrib=True -o with_cuda=True -o with_cublas=True -o with_cudnn=True -o dnn=True -o dnn_cuda=True
+conan create . --name=opencv --version=4.7.0 --user=conan-solar --channel=1_1_0 -tf "" --build=missing -s compiler.cppstd=17 -o ffmpeg/*:with_pulse=False -o ffmpeg/*:with_libfdk_aac=False -o ffmpeg/*:with_libmp3lame=False -o ffmpeg/*:with_libalsa=False -o ffmpeg/*:with_libx264=False -o ffmpeg/*:with_libx265=False -o ffmpeg/*:with_openjpeg=False
+conan create . --name=opencv --version=4.7.0 --user=conan-solar --channel=1_1_0 -tf "" --build=missing -s compiler.cppstd=17 -s build_type=Debug -o ffmpeg/*:with_pulse=False -o ffmpeg/*:with_libfdk_aac=False -o ffmpeg/*:with_libmp3lame=False -o ffmpeg/*:with_libalsa=False -o ffmpeg/*:with_libx264=False -o ffmpeg/*:with_libx265=False -o ffmpeg/*:with_openjpeg=False
+conan create . --name=opencv --version=4.7.0 --user=conan-solar --channel=1_1_0 -tf "" --build=missing -s compiler.cppstd=17 -o ffmpeg/*:with_pulse=False -o ffmpeg/*:with_libfdk_aac=False -o ffmpeg/*:with_libmp3lame=False -o ffmpeg/*:with_libalsa=False -o ffmpeg/*:with_libx264=False -o ffmpeg/*:with_libx265=False -o ffmpeg/*:with_openjpeg=False -o opencv/*:contrib=True -o opencv/*:with_cuda=True -o opencv/*:with_cublas=True -o opencv/*:with_cudnn=True -o opencv/*:dnn=True -o opencv/*:dnn_cuda=True
+conan create . --name=opencv --version=4.7.0 --user=conan-solar --channel=1_1_0 -tf "" --build=missing -s compiler.cppstd=17 -s build_type=Debug -o ffmpeg/*:with_pulse=False -o ffmpeg/*:with_libfdk_aac=False -o ffmpeg/*:with_libmp3lame=False -o ffmpeg/*:with_libalsa=False -o ffmpeg/*:with_libx264=False -o ffmpeg/*:with_libx265=False -o ffmpeg/*:with_openjpeg=False -o opencv/*:contrib=True -o opencv/*:with_cuda=True -o opencv/*:with_cublas=True -o opencv/*:with_cudnn=True -o opencv/*:dnn=True -o opencv/*:dnn_cuda=True
+
 
 
 
