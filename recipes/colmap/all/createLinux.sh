4bedab505d9b1f38bea5f4d04cd9611f8bd302c8
granger35 <jerome.royan@b-com.com>
2026-01-09 22:02:11 +0100

    remove glES2 by default for colmap create script

--- recipes/colmap/all/createLinux.sh
+++ recipes/colmap/all/createLinux.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-conan create . --name colmap --version 3.13 --user conan-solar --channel stable -tf "" --build=missing -o freeimage/*:with_openexr=False -o freeimage/*:with_tiff=False
-conan create . --name colmap --version 3.13 --user conan-solar --channel stable -tf "" --build=missing -o freeimage/*:with_openexr=False -o freeimage/*:with_tiff=False -s build_type=Debug
-conan create . --name colmap --version 3.13 --user conan-solar --channel stable -tf "" --build=missing -o freeimage/*:with_openexr=False -o freeimage/*:with_tiff=False -o *:with_cuda=True -o libglvnd/*:gles1=False
-conan create . --name colmap --version 3.13 --user conan-solar --channel stable -tf "" --build=missing -o freeimage/*:with_openexr=False -o freeimage/*:with_tiff=False -o *:with_cuda=True -o libglvnd/*:gles1=False -s build_type=Debug
+conan create . --name colmap --version 3.13 --user conan-solar --channel stable -tf "" --build=missing -o freeimage/*:with_openexr=False -o freeimage/*:with_tiff=False -o *:with_cuda=True -o libglvnd/*:gles1=False -o libglvnd/*:gles2=False
+conan create . --name colmap --version 3.13 --user conan-solar --channel stable -tf "" --build=missing -o freeimage/*:with_openexr=False -o freeimage/*:with_tiff=False -o *:with_cuda=True -o libglvnd/*:gles1=False -o libglvnd/*:gles2=False -s build_type=Debug
 
 
 
